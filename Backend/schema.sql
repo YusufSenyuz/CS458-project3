57661754564c3fc0af757b957b08de1aac250f28
Yusuf <yusuf56senyuz@gmail.com>
2024-05-02 19:08:54 +0300

    Backend from project 1 is copied

---
+++ Backend/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE user (
+    userID VARCHAR(5),
+    email VARCHAR(50),
+    password  VARCHAR(50),
+    phone VARCHAR(20),
+    PRIMARY KEY (userID)
+);
+
+insert into user values ('1','goktug@gmail.com', '123','' );
+insert into user values ('2','', '123','5301233213' );
+insert into user values ('3', 'cengizhan@gmail.com', '123', '5353211233' );
+
+
